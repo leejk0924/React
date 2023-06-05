a828d23c18dcb13b1de0eb4b0259397d7fc7f939
jk <akdirdl@gmail.com>
2023-06-05 23:40:24 +0900

    TodoEditor 컴포넌트와 css

---
+++ project1/src/component/TodoEditor.js
@@ -0,0 +1,13 @@
+import "./css/TodoEditor.css"
+const TodoEditor = () => {
+    return (
+        <div className="TodoEditor">
+            <h4>새로운 Todo 작성하기 🖊️</h4>
+            <div className="editor_wrapper">
+                <input type="text" placeholder="새로운 Todo..."/>
+                <button>추가</button>
+            </div>
+        </div>
+    );
+};
+export default TodoEditor;
