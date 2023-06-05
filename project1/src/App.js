22df5d2fdf6fc10ca74e3a4e412fa5438a6ca5fe
jk <akdirdl@gmail.com>
2023-06-05 17:32:09 +0900

    App 초기 css

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Header</div>
+            <div>Todo Editor</div>
+            <div>Todo List</div>
+        </div>
+    );
 }
 
 export default App;
